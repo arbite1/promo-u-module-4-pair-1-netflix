7229b73293eac7eefbb54352190e644581ac3bff
Ariadna Ruiz <arsarbite@gmail.com>
2023-11-06 13:35:26 +0100

    show server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // create and config server
 const server = express();
@@ -10,4 +10,7 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+server.get("/", async () => {
+  console.log(`Estoy en la ruta barrita`);
 });
